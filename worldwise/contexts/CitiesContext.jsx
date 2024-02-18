eefd53a6f1132a337f8740f53fdec13eeb84440a
Erkhes N <erkhes.nyamsaikhan@gmail.com>
2024-02-18 23:57:07 +0900

    finish workout-timer

--- worldwise/contexts/CitiesContext.jsx
+++ worldwise/contexts/CitiesContext.jsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { createContext, useEffect, useContext, useReducer } from "react";
 
 const BASE_URL = "http://localhost:8000";
@@ -66,23 +67,26 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (Number(id) === Number(currentCity.id)) return;
-    console.log(id, currentCity.id);
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === Number(currentCity.id)) return;
+      console.log(id, currentCity.id);
 
-    dispatch({ type: "loading" });
+      dispatch({ type: "loading" });
 
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: "city/loaded", payload: data });
-    } catch (err) {
-      dispatch({
-        type: "rejected",
-        payload: "There was an error loading city...",
-      });
-    }
-  }
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: "city/loaded", payload: data });
+      } catch (err) {
+        dispatch({
+          type: "rejected",
+          payload: "There was an error loading city...",
+        });
+      }
+    },
+    [currentCity.id]
+  );
 
   async function createCity(newCity) {
     dispatch({ type: "loading" });
